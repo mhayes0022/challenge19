893f9c5e9ee0dd8a73d72b4996ecfabc48382710
MHayes <mhayes0022@gmail.com>
2024-04-30 20:53:03 -0500

    working on js functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,40 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Note: from 19.28- database.js file
+
+// export const putDb = async (content) => {
+//   console.log('Post to the database');
+//   const contactDb = await openDB('contact', 1);
+//   const tx = contactDb.transaction('contact', 'readwrite');
+//   const store = tx.objectStore('contact');
+//   const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+//   const result = await request;
+//   console.log('Datas saved')
+// }
+
+//Note: from 19.24- database.js file
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+//Note: from 19.28- database.js file
+export const getDb = async () => {
+    console.log('GET from the database');
+    const contactDb = await openDB('contact', 1);
+    const tx = contactDb.transaction('contact', 'readonly');
+    const store = tx.objectStore('contact');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
 
 initdb();
